75438cfcce509a15d5fc0466cbf0e3fb4f17ebaf
José Antonio <joseantonioalmeida217@gmail.com>
2026-05-25 16:56:08 -0300

    feat: mover dados sensíveis para variáveis de ambiente
    - Mover credenciais de banco de dados para .env
    - Mover chave secreta do Flask para .env
    - Mover credenciais de email para .env
    - Criar template .env-example
    - Atualizar código para usar os.getenv()
    - Adicionar Flask-Mail às dependências

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
 app.json.sort_keys = False #type:ignore
+app.secret_key = os.getenv('secret_key', 'change-me')
 
 
 from views.customers_view import *
@@ -11,6 +16,7 @@ from views.parking_record_view import *
 from views.auth_user_view import *
 from views.parking_spot_view import *
 from views.vehicle_type_view import *
+from views.auth_view import *
 
 if __name__=='__main__':
     app.run(debug=True) #type:ignore
